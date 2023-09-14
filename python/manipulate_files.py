ff0aa95d38cb89194f1d942ee0c77bbb86526a5b
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-09-14 18:42:42 -0500

    interpret header

--- python/manipulate_files.py
+++ python/manipulate_files.py
@@ -61,3 +61,31 @@ row_is_empty : pd.Series = (
             axis = 1 ) ) # to operate on rows, not columns
 
 x = x [ ~ row_is_empty ]
+
+
+##############################
+# Create header (column names)
+##############################
+
+y = x.copy()
+
+for i in [0,1,2]:
+  y.iloc[i] = ( # Fill non-missing values forward
+    y.iloc[i] . fillna ( method = "ffill" ) )
+
+for i in [0,1,2,3]: # fill remaining missing values with ""
+                    # (which will be ignored in column names).
+  y.iloc[i] = (
+    y.iloc[i] . fillna ( "" ) )
+
+y.columns = (
+  y[0:5] # concatentate the first *four* rows
+  . apply ( ( lambda column:
+              ":".join ( [ i for i
+                           in column . astype(str)
+                           if i # drop empty strings
+                          ] ) ),
+            axis = 0 ) ) # to apply to columns, not rows
+
+# Drop the rows that defined the header.
+y = y.iloc[5:]
---
+++ python/maybe_useful.py
@@ -0,0 +1,6 @@
+# I think I don't need this but it's cool so I'm keeping it.
+
+import os
+
+# Removes the file extension
+basename = os.path.splitext ( example_file ) [0]
