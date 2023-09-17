72a3f44097b20e96ef8de042a52a8d7661293a9d
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-09-17 12:22:25 -0500

    toward putting it all together
      functionalize manipulate_files.py,
      and implement the top-level error-collection strategy

--- python/manipulate_files.py
+++ python/manipulate_files.py
@@ -2,35 +2,42 @@ import numpy as np
 import os
 import pandas as pd
 import re
-
-import python.manipulate_files_defs as defs
-
-
-example_file = "data/input/agency_responses/Unidad de información y Análisis Financiero/1.10. Formularios Planta anteproyecto 2024.xlsm UIAF.xlsm"
-
-x = pd.read_excel ( example_file )
+from   typing import Dict, List, Set, Tuple
+#
+from python.manipulate_files_defs import *
 
 
-########################
-# Start manipulating it.
-########################
+def format_tutela_response (
+    source_file : str
+) -> pd.DataFrame:
+  return (
+    false_rows_to_column_based_on_missing_values (
+      source_column_name         = "denominación de cargos:1",
+      missing_values_column_name = "grado:2",
+      new_column_name            = "empleado kind 2",
+      df = false_rows_to_column_using_regex (
+        source_column_name = "denominación de cargos:1",
+        patterns           = [ "empleado.* p.blico",
+                               "trabajador.* oficial.*", ],
+        new_column_name    = "empleado kind 1",
+        df = assemble_header (
+          strip_empty_rows (
+            strip_trailing_rows (
+              strip_leading_rows (
+                pd.read_excel (
+                  source_file ) ) ) ) ) ) ) )
 
-# Keeping a copy of each stage makes debugging easy.
-
-x0 = defs.strip_leading_rows  ( x  ) . copy()
-x1 = defs.strip_trailing_rows ( x0 ) . copy()
-x2 = defs.strip_empty_rows    ( x1 ) . copy()
-x3 = defs.assemble_header     ( x2 ) . copy()
-
-x4 = defs.false_rows_to_column_using_regex (
-  source_column_name = "denominación de cargos:1",
-  patterns           = [ "empleado.* p.blico",
-                         "trabajador.* oficial.*", ],
-  new_column_name    = "empleado kind 1",
-  df                 = x3 )
-
-x5 = defs.false_rows_to_column_based_on_missing_values (
-  source_column_name         = "denominación de cargos:1",
-  missing_values_column_name = "grado:2",
-  new_column_name            = "empleado kind 2",
-  df                         = x4 )
+def collect_formatted_responses (
+    source_files : List [str]
+) -> Tuple [ Dict [ str, pd.DataFrame ],
+             Dict [ str, Exception    ], ]:
+  errors     : Dict [ str, Exception    ] = {}
+  successes  : Dict [ str, pd.DataFrame ] = {}
+  for f in source_files:
+    try:
+      df = format_tutela_response
+    except Exception as e:
+      errors[f] = e
+    else:
+      successes[f] = df
+  return ( successes, errors )
