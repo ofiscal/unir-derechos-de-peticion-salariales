92911baa47cef403b1b97a795cecd6fcc970ea6f
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-11-29 11:50:53 -0500

    Optionally load pickled data in main.py instead of recomputing it.
    Minor bugfix: Update a variable name in a loop.

--- python/main.py
+++ python/main.py
@@ -11,13 +11,22 @@ import  python.reconnaissance.defs     as recon
 from    python.types                   import *
 
 
-recon_reports = recon.denom_cell_reports (
-  limit = None, # a limit of None (or 0) means "process everything"
-  verbose = True, )
-for k, df in recon_reports.items():
-  df.to_csv (
-    os.path.join ( "data/output",
-                   k + ".csv" ) )
+if True: # recon: Determine which sheets look friendly.
+         # THIS IS SLOW.
+  recon_reports = recon.denom_cell_reports (
+    limit = None, # a limit of None (or 0) means "process everything"
+    verbose = True, )
+
+  if False: # Maybe instead of that,
+            # simply unpickle (deserialize) saved data.
+    with open ( "pickles/Wednesday/9df594e7e714518c0763d3e5459f454cec3f408c/recon_reports.pickle",
+                "rb") as handle:
+        recon_reports = pickle . load ( handle )
+
+  for k, df in recon_reports.items():
+    df.to_csv (
+      os.path.join ( "data/output",
+                     k + ".csv" ) )
 
 instructions_for_nice_agencies = list (
   recon_reports["nice_sheets_of_agencies_with_one_nice_sheet"]
@@ -37,16 +46,16 @@ instructions_for_nice_agencies = list (
      # manually generated instructions.
      # Try the reverse and see if there are more successes that way.
      [ i for i in instructions_for_nice_agencies
-       if not k in (
+       if not i.path in (
            discoveries.exceptional_instruction_dict
            . keys () ) ]
      + discoveries.exceptional_instruction_list
    ) )
 
 for (name, obj) in [
-    ( "successes", successes ),
-    ( "errors",    errors ),
-    ( "recon_reports", recon_reports),
+    ( "successes"     , successes ),
+    ( "errors"        , errors ),
+    ( "recon_reports" , recon_reports),
 ]:
   with open( name + ".pickle", "wb") as handle:
     pickle.dump ( obj,
