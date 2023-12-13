73b182e35410fc2d72a2e602234942984c57fa8f
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-12-13 12:08:15 -0500

    Delete some obsolete exploratory code.
      (I'm keeping the function definitions it uses.)

--- python/explore/successes/main.py
+++ python/explore/successes/main.py
@@ -6,9 +6,9 @@ import pandas as pd
 import pickle
 from   typing import *
 #
+import python.explore.successes.defs as defs
 import python.paths as paths
 from   python.types import *
-import python.explore.successes.defs as defs
 
 
 successes_strategy = Definition_Strategy.Load_from_pickle
@@ -45,69 +45,3 @@ together : pd.DataFrame = defs.subset_columns_by_regex_and_concatenate (
                   for k in extra_nice },
   exprs = defs.column_name_regexes )
 
-together [ "no.*cargo.*:3$" ] = (
-  together [ "no.*cargo.*:3$" ]
-  . replace ( " ", np.nan )
-  . astype ( float ) )
-
-together [ ".*denom.*cargo.*denom.*cargo.*denom.*cargo.*denom.*cargo.*" ] # verbal
-together [ "grado[^-]*" ] .  unique() # messy
-together [ "no.*cargo.*:3$" ] . replace ( " ", np.nan ) # float
-
-x = together [ "salario.*comun.*subtotal.*" ] . astype ( str )
-x [ x.apply ( len ) < 1 ]
-
-together [ "salario.*comun.*subtotal.*" ] . replace ( "", np.nan ) . astype (float) # bad -- want the total, not the subtotal
-together [ ".*remuneraciones.*remun.*subtotal.*" ] # bad -- want the total, not the subtotal
-together [ ".*inherentes.*total.*10" ]
-together [ "prestac.*social.*relac.*total.*" ]
-together [ ".*total.*:.*gastos.*:.*personal.*" ]
-
-
-###############
-# Futz around #
-###############
-
-for col_name in together.columns:
-  print()
-  print(col_name)
-  print( together[col_name].unique() )
-
-print ( "Files successfully read: ",
-        len ( colnames_by_file["file"] . unique() ) )
-
-print ( "Unique column names: ",
-        len ( colnames_by_file["column"] . unique() ) )
-
-# An exemplar: The first element of successes.
-k0 = list(successes.keys()) [0]
-v0 = successes[k0]
-c0 = list(v0.columns)
-
-# Pretty good -- 114 have well-behaved column names.
-for expr in defs.column_name_regexes:
-  print ()
-  defs.summarize_expr_in_column_names (
-    colnames_by_file = colnames_by_file,
-    expr             = expr)
-
-expr = ".*total.*:.*gastos.*:.*personal.*"
-defs.summarize_expr_in_column_names (
-  colnames_by_file   = colnames_by_file,
-  expr               = expr )
-
-( defs.matches_in_spreadsheets_with_multiple_matches
-  ( colnames_by_file = colnames_by_file,
-    expr             = expr ) )
-( defs.matches_in_spreadsheets_with_multiple_matches
-  ( colnames_by_file = colnames_by_file,
-    expr             = expr )
-  ["column"] . unique() )
-( defs.matches_in_spreadsheets_with_multiple_matches
-  ( colnames_by_file = colnames_by_file,
-    expr             = expr )
-  ["file"] . unique() )
-
-df = defs.subset_columns_by_regex_and_concatenate (
-  dfs_by_file = successes,
-  exprs       = column_name_regexes )
